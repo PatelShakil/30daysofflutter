7943ac91d5cf9185dec17244f1d407313c6395d7
PatelShakil <patelsakib95@gmail.com>
2023-01-15 19:25:45 +0530

    day 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/services/auth_services.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase . initializeApp (
+    options : DefaultFirebaseOptions . currentPlatform ,
+  );
   runApp(MyApp());
 }
 
@@ -13,17 +23,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-      themeMode: ThemeMode.light,
-      darkTheme: ThemeData(brightness: Brightness.light),
-      theme: ThemeData(
-        fontFamily: GoogleFonts.lato().fontFamily,
+    return MultiProvider(
+      providers:[
+        Provider<AuthService>(
+          create:(_) => AuthService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthService>().authStateChanges,
+          initialData: null,
+        ),
+      ],
+      child: MaterialApp(
+        title: "APP",
+        home:AuthWrapper()
       ),
-      routes: {
-        MyRoutes.loginRoute: (context) => LoginPage(),
-        MyRoutes.homeRoute: (context) => Homepage(),
-      },
     );
+  }
+}
+class AuthWrapper extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   final user = context.watch<User?>();
+   if(user != null){
+     return Homepage();
+   }else {
+     return LoginPage();
+   }
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/services/auth_services.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,61 +16,82 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    String name = "";
-    return Material(
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
+    var email = TextEditingController();
+    var password = TextEditingController();
+    var name = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "LOGIN",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.blueAccent,
+                fontFamily: GoogleFonts.tajawal().fontFamily,
               ),
-              Text(
-                "Welcome $name",
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold),
+            ),Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: TextFormField(
+                controller: name,
+                decoration: InputDecoration(
+                    hintText: "Enter Your name", label: Text("Name")),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 10.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter Username",
-                        labelText: "Username",
-                      ),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                      ),
-                      onChanged: (value) {},
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    ElevatedButton(
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      child: Text("Login"),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                    hintText: "Enter Email", label: Text("Email")),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: TextFormField(
+                controller: password,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", label: Text("Password")),
+              ),
+            ),
+            Container(
+              height: 30,
+              width: 100,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (!email.text.isEmpty || !password.text.isEmpty) {
+                    context
+                        .read<AuthService>()
+                        .login(email.text.trim(), password.text.trim());
+                  }
+                },
+                child: Text("Login"),
+              ),
+            ),
+            Container(
+              height: 30,
+              width: 100,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (!email.text.isEmpty || !password.text.isEmpty) {
+                    context
+                        .read<AuthService>()
+                        .signup(name.text.trim(),email.text.trim(), password.text.trim());
+                  }
+                },
+                child: Text("Signup"),
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(),
+    );
   }
 }
